3d3a5619ebf1aae2421e7a5bbf38ccdaaa890220
Michael Feihstel <michaelfeihstel@gmail.com>
2013-10-12 17:51:31 +0200

    config.assets.initialize_on_precompile set to false

--- config/application.rb
+++ config/application.rb
@@ -53,6 +53,6 @@ module Filialen
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    config.assets.initialize_on_precompile = false
+    # config.assets.initialize_on_precompile = false
   end
 end
