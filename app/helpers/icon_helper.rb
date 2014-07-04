972c44fccc1fe6eba5430e033ff1599ae7da50c5
Michael Feihstel <michaelfeihstel@gmail.com>
2014-07-04 14:16:01 +0200

    styling

--- app/helpers/icon_helper.rb
+++ app/helpers/icon_helper.rb
@@ -14,4 +14,23 @@ module IconHelper
 		content_tag("span", "", class: "square_icon #{icon}")
 	end
 
+	def icon(icon, options={})
+		color_attribute = "style='color: #{options[:color]}'" unless options[:color].nil?
+		size_attribute = "style='size: #{options[:size]}'" unless options[:size].nil?
+		icon_class = "icon-#{icon}"
+		additional_classes = options[:class] || nil
+		text = " #{options[:text]}" || ""
+
+		span_tag = content_tag(:span, text, class: "#{icon_class} #{additional_classes}")
+		span_tag.html_safe
+	end
+
+	def boolean_icon(value)
+		if value == true
+			icon("check-square", color: "#00D300", class: "right")
+		else
+			icon("cross-square", color: "#FF766F", class: "right")
+		end
+	end
+
 end
