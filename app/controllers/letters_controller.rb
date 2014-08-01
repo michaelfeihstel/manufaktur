c9385eaecbc0483f59987a2e07765d2c0d96029c
Michael Feihstel <michaelfeihstel@gmail.com>
2014-08-01 14:06:26 +0200

    further redesign

--- app/controllers/letters_controller.rb
+++ app/controllers/letters_controller.rb
@@ -21,11 +21,35 @@ class LettersController < ApplicationController
 		@letter = Letter.find(params[:id])
 	end
 
+	def update
+		@letter = Letter.find(params[:id])
+		if @letter.update_attributes(letter_params)
+			redirect_to letter_path(@letter), notice: "Brief aktualisiert"
+		else
+			render "edit"
+		end
+	end
+
 
 
 	private
 	def initialize_search
 		@search = Letter.search(params[:q])
+	end
+
+	def letter_params
+		params.require(:letter).permit(
+			:contact_id,
+			:address_id,
+			:name,
+			:street,
+			:house_number,
+			:zip,
+			:city,
+			:country,
+			:subject,
+			:body
+		)
 	end
 
 end
