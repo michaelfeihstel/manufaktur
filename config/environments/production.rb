d07b013171bf81de910a814d63519009e334917e
Michael Feihstel <michaelfeihstel@gmail.com>
2015-06-30 08:32:12 +0200

    enable asset compression

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,6 +21,9 @@ Rails.application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  # Generate digests for assets URLs
+  config.assets.compress = true
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
