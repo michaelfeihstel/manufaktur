7513c03e1250675703e24245840a21e7094a68e7
Michael Feihstel <michaelfeihstel@gmail.com>
2015-07-06 11:48:20 +0200

    visual fixes

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,15 +1,15 @@
 module ProductsHelper
-	def product_color(product, border_align: "bottom")
-		c1 = product.primary_color.to_s || "#F2F2F2"
-		c2 = product.secondary_color || "#455A64"
-		c3 = product.text_color || "#FFF"
+  def product_label_style(product, border_align: "bottom")
+    c1 = product.primary_color.blank? ? "#F2F2F2" : product.primary_color
+    c2 = product.secondary_color.blank? ? "#E3E3E3" : product.secondary_color
+    c3 = product.text_color || "#333"
 
-		back = "background-color: #{c1};"
-		border = "border-#{border_align}: 4px solid #{c2};"
-		text = "color: #{c3};"
+    back = "background-color: #{c1};"
+    border = "border-#{border_align}: 4px solid #{c2};"
+    text = "color: #{c3};"
 
-		"#{back} #{border} #{text}"
-	end
+    "#{back} #{border} #{text}"
+  end
 
   def horizontal_gradient(product)
     primary_color = product.primary_color
@@ -32,6 +32,8 @@ module ProductsHelper
     secondary_color = product.secondary_color
     text_color = product.text_color
 
-    "background-color: #{primary_color}; border-bottom: 5px solid #{secondary_color};"
+    unless primary_color.empty? || secondary_color.empty? || text_color.empty?
+      "background-color: #{primary_color}; border-bottom: 5px solid #{secondary_color};"
+    end
   end
 end
