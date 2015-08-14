ee1d02b67faee3f354ae242a8fa8af96571d8bef
Michael Feihstel <michaelfeihstel@gmail.com>
2015-08-14 09:08:12 +0200

    clean up gems

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,18 +5,9 @@ class ApplicationController < ActionController::Base
   # include helpers
   helper :all
 
-  # filter
-  before_filter :store_history
-
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   private
-
-  def store_history
-    session[:history] ||= []
-    session[:history].delete_at(0) if session[:history].size >= 5
-    session[:history] << request.url
-  end
 
   def user_not_authorized
     flash[:error] = "Sie müssen angemeldet sein um diese Aktion durchzuführen."
