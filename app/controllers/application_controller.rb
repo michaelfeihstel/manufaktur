5bd2814eacb7660f879f2a832000be91aaf0646d
Michael Feihstel <michaelfeihstel@gmail.com>
2015-09-14 11:25:25 +0200

    add rack mini profiler

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery
 
+  before_action :check_rack_mini_profiler
+
   # include helpers
   helper :all
 
@@ -12,6 +14,13 @@ class ApplicationController < ActionController::Base
   def user_not_authorized
     flash[:error] = "Sie müssen angemeldet sein um diese Aktion durchzuführen."
     redirect_to(request.referrer || new_user_session_path)
+  end
+
+  def check_rack_mini_profiler
+    # for example - if current_user.admin?
+    if params[:rmp]
+      Rack::MiniProfiler.authorize_request
+    end
   end
 
 end
