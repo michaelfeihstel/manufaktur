257c46b17065346fd7083d6b7c2561d342728813
Michael Feihstel <michaelfeihstel@gmail.com>
2016-08-04 15:53:31 +0200

    fix display error for charts in safari

--- app/assets/javascripts/application-track.js
+++ app/assets/javascripts/application-track.js
@@ -5,9 +5,3 @@ $(".breadcrumbs-container").stick_in_parent({
   offset_top: 55,
   bottoming: false
 });
-
-$(document).on('turbolinks:load', function() {
-   if (typeof chart !== 'undefined') {
-     chart.resize();
-   }
-});
