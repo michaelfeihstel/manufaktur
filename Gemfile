651ffdf6421152c80737e6ef0d7430fa62c2766b
Michael Feihstel <michaelfeihstel@gmail.com>
2018-12-10 10:42:39 +0100

    update gem bundle

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 
 ruby '2.4.1'
 
-gem 'rails', '~> 5.1'
+gem 'rails', '~> 5.2'
 gem 'pg', '~> 0.21'
 gem 'turbolinks'
 gem 'annotate'
--- config/initializers/timeout.rb
+++ config/initializers/timeout.rb
@@ -1,2 +1,20 @@
-Rack::Timeout.timeout = 25  # seconds
-Rack::Timeout::Logger.disable
+class TimeoutRecovery
+  def initialize(app, options = {})
+    @app, @options = app, options
+  end
+
+  # Rack::Timeout, as of 0.2.4, raises errors that descend from the Exception
+  # class when a request takes too long - however, this is a very strong
+  # exception level and will kill Puma's worker if we let it continue down the
+  # stack. So instead, catch it and wrap it in a recoverable StandardError.
+
+  class Error < StandardError; end
+
+  def call(env)
+    @app.call(env)
+  rescue Rack::Timeout::Error => e
+    raise Error, "Original Error: #{e.class.inspect}, #{e.message.inspect}, #{e.backtrace.inspect}"
+  end
+end
+
+Rails.application.config.middleware.insert_before Rack::Runtime, Rack::Timeout, service_timeout: 20
